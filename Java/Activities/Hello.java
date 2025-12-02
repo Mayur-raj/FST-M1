ae0e0f3f5704220ec124a106b16731404f00ab91
Mayursing Rajput <73985660+Mayur-raj@users.noreply.github.com>
2025-12-02 08:48:14 +0530

    Add files via upload

---
+++ Java/Activities/Hello.java
@@ -0,0 +1,25 @@
+package example;
+
+public class Hello {
+	
+	public static void main(String[]agr){
+		
+		/*int a = 10;
+		float f = 10.10f;
+		char c = 'M';
+		String S = "Mayur";
+		System.out.println("Hello " + S);
+		System.out.println(S.length());
+		*/
+		
+		int x=13;
+		if (x % 2 == 0) {
+			System.out.println("X is Eeven Number");
+		}
+		
+		else
+			System.out.println("X is Odd Number");
+		
+	}
+
+}
