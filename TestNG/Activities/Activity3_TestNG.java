d4bfc72f46f4b8bd1fab31a7fcb6e98a556c67a8
Mayursing Rajput <73985660+Mayur-raj@users.noreply.github.com>
2025-12-17 10:46:01 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity3_TestNG.java
@@ -0,0 +1,39 @@
+package TestNG_Activity;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Activity3_TestNG {
+    WebDriver driver;
+
+    @BeforeClass
+    public void beforeClass() {
+        driver = new FirefoxDriver();
+        driver.get("https://training-support.net/webelements/login-form");
+    }
+
+    @Test
+    public void loginTest() {
+        WebElement username = driver.findElement(By.id("username"));
+        WebElement password = driver.findElement(By.id("password"));
+
+        username.sendKeys("admin");
+        password.sendKeys("password");
+
+        driver.findElement(By.xpath("//button[text()='Submit']")).click();
+
+        String loginMessage = driver.findElement(By.cssSelector("h2.text-center")).getText();
+        Assert.assertEquals("Welcome Back, Admin!", loginMessage);
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.close();
+    }
+}
